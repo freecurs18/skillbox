a0c35a8e21bf4efea1e14aa00dad84f03f60e56e
freecurs18 <boroda297@gmail.com>
2022-06-14 17:28:55 +0300

    Решил 3 практику

--- skillbox/praktika_3.py
+++ skillbox/praktika_3.py
@@ -2,6 +2,10 @@ one_kvartal = int(input('Введите доход первого квартал
 two_kvartal = int(input('Введите доход второго квартала '))
 three_kvartal = int(input('Введите доход третьего квартала '))
 four_kvartal = int(input('Введите доход четвёртого квартала '))
+first_six_monthe = one_kvartal + two_kvartal
+second_six_monthe = three_kvartal + four_kvartal
+razniza_megdu_kvartalavi = first_six_monthe / second_six_monthe
 print ('=======================')
-print ('Суммма первого и второго квартала' , one_kvartal + two_kvartal, 'руб')
-print ('Суммма третьего и четвёртого квартала', three_kvartal + four_kvartal, 'руб')
+print ('Суммма первого и второго квартала' , first_six_monthe, 'руб')
+print ('Суммма третьего и четвёртого квартала', second_six_monthe, 'руб')
+print ('Отношение первого квартала ко второму',razniza_megdu_kvartalavi)
